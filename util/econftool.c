f465e63b782517f8cbc96fff5ee457bf8db50e1a
Jorik Cronenberg <jcronenberg@suse.de>
2020-03-17 09:33:19 +0100

    Remove unnecessary includes

--- util/econftool.c
+++ util/econftool.c
@@ -21,13 +21,10 @@
 
 #include <errno.h>
 #include <getopt.h>
-#include <pwd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <limits.h>
 
